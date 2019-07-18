be2c4529a45a395d587891210eb9d59cd8cd60f6
duong mau <dpm11234@gmail.com>
2019-07-18 13:30:28 +0700

    done footer

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Footer from './../../layout/footer/Footer';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home Works!!
+        <div className="row">
+          <div className="col-lg-12">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
