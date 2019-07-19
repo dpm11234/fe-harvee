3499f4c486df90bbb71e3b00888b8fd7326a3d85
duong mau <dpm11234@gmail.com>
2019-07-19 23:49:23 +0700

    config redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,9 @@
-import {  } from 'redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+
+const store = createStore({
+   rootReducer
+});
 
 
+export default store;
