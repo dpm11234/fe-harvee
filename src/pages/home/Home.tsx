837e3d41756f539750c7639daffb76d5578ddd2b
tuankhoi911 <17520648@gm.uit.edu.vn>
2019-07-22 15:50:01 +0700

    carousel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import Footer from './../../layout/footer/Footer';
+import Navbar from '../../layout/navbar/Navbar';
+import CarouselComponet from '../../components/carousel/Carousel';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        Home Works!!
-        <div className="row">
-          <div className="col-lg-12">
-            <Footer />
-          </div>
+        <div>
+          <Navbar />
         </div>
+        <div className="hv-home">
+          <CarouselComponet />
+        </div>
+        <Footer />
       </div>
     );
   }
