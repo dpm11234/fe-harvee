9cd8e50562fe93c45ef30294f1391cc43320e584
duong mau <dpm11234@gmail.com>
2019-07-22 15:53:00 +0700

    add router, change .tsx to .jsx

---
+++ src/layout/Client/AppClient.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './../../pages/home/Home';
+import Contact from '../../pages/contact/Contact';
+import Footer from '../../components/footer/Footer';
+import Categories from '../../pages/categories/Categories';
+
+class AppClient extends Component {
+   render() {
+      return (
+         <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/categories" component={Categories} />
+
+            <Footer />
+         </div>
+      );
+   }
+}
+
+export default AppClient;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import Footer from './../../layout/footer/Footer';
+import Footer from './../../components/footer/Footer';
 
 class Home extends Component {
   render() {
     return (
       <div>
         Home Works!!
-        <div className="row">
-          <div className="col-lg-12">
-            <Footer />
-          </div>
-        </div>
       </div>
     );
   }
