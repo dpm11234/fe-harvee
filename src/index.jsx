cf03473b9922f18e1822a921d130820690b8bca8
tuankhoi911 <17520648@gm.uit.edu.vn>
2019-07-23 11:48:43 +0700

    merge

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,6 @@ import { render } from 'react-dom';
 import "jquery/dist/jquery.slim"
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-=======
-import './scss/main.scss';
->>>>>>> 3248134c29f76f4a3cb3696a52bbf86fc0fec699
 
 import './scss/main.scss';
 import './style.scss';
