de8407a3e846e6913a975809678c8b7974c1c087
duong mau <dpm11234@gmail.com>
2019-07-24 10:35:46 +0700

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import AppClient from './layout/Client/AppClient';
 import AppAdmin from './layout/AppAdmin';
+import Home from './pages/home/Home';
+import Categories from './pages/categories/Categories';
+import Contact from './pages/contact/Contact';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={AppClient} />
+        <Route exact path="/" render={() => {
+          return (
+            <Redirect to="/home" />
+          );
+        }} />
+
+        <Route exact path="/home" render={() => {
+          return (
+            <AppClient>
+              <Home />
+            </AppClient>
+          );
+        }} />
+
+        <Route exact path="/category" render={() => {
+          return (
+            <AppClient>
+              <Categories />
+            </AppClient>
+          );
+        }} />
+
+        <Route exact path="/contact" render={() => {
+          return (
+            <AppClient>
+              <Contact />
+            </AppClient>
+          );
+        }} />
+
+
         <Route path="/admin" component={AppAdmin} />
 
       </Router>
--- src/layout/Client/AppClient.jsx
+++ src/layout/Client/AppClient.jsx
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import Home from './../../pages/home/Home';
-import Contact from '../../pages/contact/Contact';
+// import { Route, Redirect } from 'react-router-dom';
+
 import Footer from '../../components/footer/Footer';
-import Categories from '../../pages/categories/Categories';
 import Navbar from '../../components/navbar/Navbar';
-import ProdDetail from '../../components/prod-detail/ProdDetail';
+
+
 
 class AppClient extends Component {
+
    render() {
       return (
          <div>
             <Navbar />
-
-            <Route path="/" exact render={() => <Redirect to="/home" />} />
-            <Route path="/home" component={Home}/>
-            <Route path="/contact" component={Contact} />
-            <Route path="/category" component={Categories} />
-            <Route path="/detail" component={ProdDetail} />
-
+            {this.props.children}
             <Footer />
          </div>
       );
